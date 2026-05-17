c4c5907908e0a6c69d1c2492adbdc64f8e5f6214
palink <1006282023@qq.com>
2026-05-17 22:43:52 +0800

    test: make server smoke test build host

--- tests/LightRAGNet.Server.Tests/ServerHostSmokeTests.cs
+++ tests/LightRAGNet.Server.Tests/ServerHostSmokeTests.cs
@@ -1,4 +1,7 @@
 using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace LightRAGNet.Server.Tests;
@@ -6,8 +9,29 @@ namespace LightRAGNet.Server.Tests;
 public class ServerHostSmokeTests
 {
     [Fact]
-    public void TestProject_Loads()
+    public void ServerHost_CanBeCreated()
     {
-        typeof(Program).Assembly.GetName().Name.Should().Be("LightRAGNet.Server");
+        using var factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((_, configuration) =>
+                {
+                    configuration.AddInMemoryCollection(new Dictionary<string, string?>
+                    {
+                        ["ConnectionStrings:DefaultConnection"] = "Data Source=:memory:",
+                        ["LLM:ApiKey"] = "test-key",
+                        ["Embedding:ApiKey"] = "test-key",
+                        ["Rerank:ApiKey"] = "test-key",
+                        ["Neo4j:Uri"] = "neo4j://localhost:7687",
+                        ["Neo4j:User"] = "neo4j",
+                        ["Neo4j:Password"] = "test-password",
+                        ["LightRAG:WorkingDir"] = "rag_storage_test"
+                    });
+                });
+            });
+
+        using var client = factory.CreateClient();
+
+        client.Should().NotBeNull();
     }
 }
